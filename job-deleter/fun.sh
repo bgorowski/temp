6bb9033c027c9e6fc893f7b5544bd931696f91bc
bgorowski <bgorowski@gmail.com>
2024-01-19 17:06:04 +0100

    change get_maxruns

--- job-deleter/fun.sh
+++ job-deleter/fun.sh
@@ -12,3 +12,14 @@ truncate_whitespaces()
 
 result=`truncate_whitespaces "jeden               dwa  trzy   ."`
 echo $result
+
+test()
+{
+    local maxrun_time_dirty="07:23:41]"
+
+    maxrun_time_dirty=${maxrun_time_dirty%?}
+
+    echo $maxrun_time_dirty
+}
+
+test
